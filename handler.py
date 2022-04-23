7939f3163a5b6a605764ddeb307eb9919951d292
Roman3214 <87423013+Roman3214@users.noreply.github.com>
2022-04-23 16:24:55 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -17,31 +17,6 @@ from test_db import users_id_Pogr, users_id_Detsk, users_id_Narkolog, ALL
 import os
 import time
 
-'''
-#for i in len(users_id_db):
-async def send_input_file(dp):
-  #await bot.send_file(chat_id= users_id_db[const])    
-    const = 0
-    while const < len(users_id_db):
-        chat_id = int(users_id_db[const])
-        photo = open("F:\\фотки\\2880x1800_799985_[www.ArtFile.ru].jpg", 'rb')
-        caption = "it's just photo, don't worry"
-        await bot.send_photo(chat_id, photo, caption)    
-        const += 1
-
-async def send_input_photo(dp):
-  #await bot.send_file(chat_id= users_id_db[const])    
-    #const = 0
-    #while const < len(users_id_db):
-    chat_id = admin_id
-    photo = open("F:\\фотки\\bg-03.jpg", 'rb')
-    caption = "it's just photo, don't worry"
-    await bot.send_photo(chat_id, photo, caption)    
-     #   const += 1'''
-'''
-
-'''
-
 @dp.message_handler(commands=['start'])
 async def Keyboard_Button(message: Message):
     global button
@@ -53,7 +28,7 @@ async def Keyboard_Button(message: Message):
 async def other_command(message: Message):
     next_id = message.message_id
     print(next_id)
-    # хуета полная
+  
     
 async def send_object(id):
     try:
@@ -74,7 +49,6 @@ async def send_object(id):
         checkid = cursors.execute('SELECT * FROM datainfo ')
         checkid_end = [q for q in checkid]
         connn.commit()
-        #connn.close()
     except sqlite3.Error as error:
         print('Error', error)
     finally:
@@ -90,8 +64,7 @@ async def send_object(id):
                     await bot.send_message(int(id), str(send_object))
                     sql_update_query = """DELETE FROM datainfo WHERE text_message = ?"""
                     cursor.execute(sql_update_query, (send_object, ))
-                    conn.commit()       
-                    print("Соединение с SQLite закрыто1")        
+                    conn.commit()        
                 elif text_end[0] == None:
                     break
 
@@ -100,8 +73,7 @@ async def send_object(id):
                     await bot.send_photo(id, open(send_object, 'rb'))
                     sql_update_query = """DELETE FROM datainfo WHERE photo_message = ?"""
                     cursor.execute(sql_update_query, (send_object, ))
-                    conn.commit()
-                    print("Соединение с SQLite закрыто2")  
+                    conn.commit
                 if photo_end[0] == None:
                     break    
 
@@ -110,8 +82,7 @@ async def send_object(id):
                     await bot.send_video(id, open(send_object, 'rb'))
                     sql_update_query = """DELETE FROM datainfo WHERE video_message = ?"""
                     cursor.execute(sql_update_query, (send_object, ))
-                    conn.commit()
-                    print("Соединение с SQLite закрыто3")       
+                    conn.commit()    
                 if video_end[0] == None:
                     break
 
@@ -188,183 +159,6 @@ async def check_send_message_txt(dp):
                 for id in users_id_Narkolog:
                     await send_object(id)  
             break
-                #print(media)
-                #await bot.send_media_group(id, media)   
-'''
-                try:
-                    connn = sqlite3.connect('users.db')
-                    cursors = connn.cursor()
-                    text = cursors.execute('SELECT * FROM datainfo ORDER BY ROWID ASC LIMIT 1')#SELECT * FROM SAMPLE_TABLE ORDER BY ROWID ASC LIMIT 1
-                    e = [q for q in text]
-                    #print(e[0][1])
-                    connn.commit()
-                    #connn.close()
-                except sqlite3.Error as error:
-                    print('Error', error)
-                finally:
-                    if(connn):
-                        connn.close()
-                try:        
-                    count = 0
-                    for send_object in e[0]:
-                        if send_object != None:
-                            
-                            try:
-                                conn = sqlite3.connect('users.db')
-                                cursor = conn.cursor()
-                                if count == 0:
-                                    print('0')
-                                    '''
-'''
-                                    with open('tee.txt', 'w', encoding= 'utf-8') as read_file:
-                                        tex = read_file.write(send_object)
-                                    with open('tee.txt', 'r', encoding= 'utf-8') as read_file:
-                                        textii = read_file.read()'''
-                                        
-'''
-                                    await asyncio.sleep(3)
-                                    await bot.send_message(int(id), str(send_object))
-                                    #time.sleep(1)
-                                    await asyncio.sleep(3)
-                                    sql_update_query = """DELETE FROM datainfo WHERE text_message = ?"""
-                                    cursor.execute(sql_update_query, (send_object, ))
-                                    conn.commit()
-                                elif count == 1:
-                                    print('1')
-                                    await asyncio.sleep(3)
-                                    await bot.send_photo(id, open(send_object, 'rb'))
-                                    await asyncio.sleep(3)
-                                    sql_update_query = """DELETE FROM datainfo WHERE photo_message = ?"""
-                                    cursor.execute(sql_update_query, (send_object, ))
-                                    conn.commit()
-                                elif count == 2:
-                                    print('2')
-                                    await asyncio.sleep(3)
-                                    await bot.send_video(id, open(send_object, 'rb'))
-                                    await asyncio.sleep(3)
-                                    sql_update_query = """DELETE FROM datainfo WHERE video_message = ?"""
-                                    cursor.execute(sql_update_query, (send_object, ))
-                                    conn.commit()
-                                elif count == 3:
-                                    print('3')
-                                    await asyncio.sleep(3)
-                                    await bot.send_document(id, open(send_object, 'rb'))
-                                    await asyncio.sleep(3)
-                                    sql_update_query = """DELETE FROM datainfo WHERE documentation_message = ?"""
-                                    cursor.execute(sql_update_query, (send_object, ))
-                                    conn.commit()
-                                
-                                    
-                            except sqlite3.Error as error:
-                                print("Ошибка при работе с SQLite", error)
-                            finally:
-                                if conn:
-                                    conn.close()
-                                    print("Соединение с SQLite закрыто")
-                        count +=1
-                        if count == 4:
-                            count = 0
-                except IndexError as error:
-                    print('the list is empty ')
-                    break'''
-                #await bot.send_photo(i, poh)
-                #time.sleep(1)
-                #os.remove(list_photot()[count1])
-                #count1 +=1
-'''
-            q = 0
-            for r in range(5):
-                if q < len(list_photot()):
-                    with open(list_photot()[q], 'rb') as send:
-                        poh = send.read()
-                q +=1
-            count1 = 0
-'''
-            #for lp in list_photot():
-            #if list_photot() == []:    
-            #    os.remove('textALL.txt')
-
-
-'''
-        elif os.path.isfile('textP.txt') == True:
-            with open('textP.txt', 'r', encoding= 'utf-8') as read_file:
-                tex = read_file.read()
-            for i in users_id_Pogr:
-                await bot.send_message(chat_id=i, text= tex)
-            os.remove('textP.txt')
-            
-        elif os.path.isfile('textD.txt') == True:
-            with open('textD.txt', 'r', encoding= 'utf-8') as read_file:
-                tex = read_file.read()
-            for i in users_id_Detsk:
-                await bot.send_message(chat_id=i, text= tex)
-            os.remove('textD.txt')
-            
-        elif os.path.isfile('textN.txt') == True:
-            with open('textN.txt', 'r', encoding= 'utf-8') as read_file:
-                tex = read_file.read()
-            for i in users_id_Narkolog:
-                await bot.send_message(chat_id=i, text= tex)
-            os.remove('textN.txt')
-        elif e[0][0] == None:
-            break
-'''
-        
-
-        
-            
-        
-
-async def send_input_photo(dp):
-    chat_id = admin_id
-    list_format_photo = ['.raw', '.jpeg', '.jpg', '.tiff', '.psd', '.bmp', '.png', '.gif']
-    
-    dir = os.listdir()
-    
-    list_path_photo = []
-    
-    q = 0 
-    for i in list_format_photo:
-        for photo in dir:
-            #photo = str(photo).lower()
-            if str(photo).lower().find(i) != -1:
-                list_path_photo.append(photo)
-                print(list_path_photo)
-                with open(list_path_photo[q], 'rb') as send:
-                    await bot.send_photo(chat_id, send)
-                #print(list_path[q])
-                q +=1 
-                break
-    pass
-        #await bot.send_photo(chat_id, e)
-                #list_path.append(photo)
-                
-                #print(photo) 
-    
-    #photo = open("H:\\фотки\\bg-03.jpg", 'rb')
-    #caption = "it's just photo, don't worry"
-    #await bot.send_photo(chat_id, list_path)#, caption)
-
-
-async def send_documents(dp):
-    pass  
-    #await bot.send_document() 
-     
-
-'''
-async def send_to_admin(dp):
-    const = 0
-    while const < len(users_id_db):
-        try:
-            await bot.send_message(chat_id=int(users_id_db[const]), text='thanks for using my bot :)') #579244655,text="Please off sound") 
-        except aiogram.utils.exceptions.BotBlocked:
-            print(f'User nuber {const} deleted the bot ')
-    #except NameError:
-    #    print(f'User nuber {const} deleted the bot ')
-        const += 1
-        # def send-message need to try: except: Error
-
-'''
 
 @dp.callback_query_handler(lambda c: c.data == 'button1')
 async def process_callback_boundary_conditions_department(callback_query: CallbackQuery):
@@ -459,36 +253,6 @@ async def process_callback_narcology(callback_query: CallbackQuery):
     
     await bot.send_message(callback_query.from_user.id, text)
     await button.delete()
-"""
-@dp.message_handler(lambda c:True, content_types=['text'])#этот блок выполнится если юзер отправит боту сообщение
-def info_message(message: Message):
-    bot.edit_message_reply_markup(message.chat.id, message_id = message.message_id-1, reply_markup = '')# удаляем кнопки у последнего сообщения
-
-"""
 @dp.message_handler(commands=['help'])
 async def send_help(message: Message):
     await message.reply("Чем я могу помочь?")
-
-'''
-@dp.message_handler()
-async def echo (message: Message):
-    text = f' "{message.from_user.first_name}" Привет ты написал мне: {message.text}, this is you username :"{message.from_user.username}"'
-    print(message, text)
-    #await bot.send_message(chat_id=message.from_user.id, text=text)
-    await message.answer(text=text)
-'''
-'''   
- {"message_id": 665, "from":
-    {"id": 584914872, 
-        "is_bot": false,
-        "first_name": "чех", 
-        "username": "cheh102", 
-        "language_code": "ru"}, 
-    "chat": {"id": 584914872, 
-        "first_name": "чех", 
-        "username": "cheh102", 
-        "type": "private"}, 
-    "date": 1644235891, 
-    "text": "Привет"} 
-     "чех" Привет ты написал мне: Привет
-'''
