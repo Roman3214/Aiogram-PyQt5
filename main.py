b93992a16b09ace593d2af4cc8542918d8c32699
Roman3214 <87423013+Roman3214@users.noreply.github.com>
2022-04-23 16:25:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher, executor
 from config import BOT_TOKEN 
-#from handler import main+
-
 
 loop = asyncio.get_event_loop()
 bot = Bot(BOT_TOKEN, parse_mode='HTML')
@@ -20,23 +18,4 @@ if __name__ == '__main__':
     #lists_function = [send_input_photo, check_send_message_txt ]
     executor.start_polling(dp, on_startup = check_send_message_txt)
    
-    '''for i in range(1):
-        my_thread = Thread(target=main(), args=(i,))
-        my_thread_two = Thread(target=start_tegram(), args=(i,))
-        my_thread.start() and         my_thread_two.start()
-       '''
-      
-      
-    #main()
-    #from handler import dp#, main#, send_to_admin#,  send_input_file #send_to_admin
-    #executor.start_polling(dp, on_startup=)
-    #main()
-    #for i in range(1):
-    #    my_thread = Thread(target=main(), args=(i,))
-        #my_thread.start()
-    #    executor.start_polling(dp)
-    #    
-    #main()
-      #, on_startup=send_to_admin)#, on_startup=send_input_file)#send_to_admin, send_input_text)
-    
-    
+  
