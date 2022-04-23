e3d77f885b29b822c72553c358460a5cc9ef1e47
Roman3214 <87423013+Roman3214@users.noreply.github.com>
2022-04-23 16:20:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = '0000000000:AAAAAAAA...' #youк token
-admin_id = '000000000' #youк id telegram
+BOT_TOKEN = '0000000000:AAAAAAAA...' #your token
+admin_id = '000000000' #your id telegram
 BOT_URL = "https://api.telegram.org/bot..." # your bot url
 
